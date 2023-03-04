2e6f01484b57dae33283952c17bc701fc199bf6a
pcKaliper <gxlthz@gmail.com>
2023-03-04 16:42:10 -0500

    sideBar done

--- Proyectos/Los Simpons/script.js
+++ Proyectos/Los Simpons/script.js
@@ -11,4 +11,24 @@ favButton.forEach((item) => {
 });
 // -----------------------------------------------------------------------------
 
+// Sidebar
+const menuToggle = document.querySelector(".menuToggle");
+const nav = document.querySelector(".navegation");
 
+menuToggle.onclick = () => {
+	nav.classList.toggle("open");
+};
+
+const list = document.querySelectorAll(".list");
+
+function activarLink() {
+	list.forEach((item) => item.classList.remove("active"));
+	this.classList.add("active");
+
+
+}
+
+list.forEach((item) => 
+item.addEventListener("click", activarLink)
+
+)
